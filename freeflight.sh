30998d3cbb377cd3fa42f91470484005b8233e10
jethr0-1 <66189463+jethr0-1@users.noreply.github.com>
2021-09-24 16:45:21 +1000

    Update freeflight.sh

--- freeflight.sh
+++ freeflight.sh
@@ -1,3 +1,5 @@
+  GNU nano 3.2                                                                   freeflight.sh                                                                              
+
 clear
 echo '      ::::::::::::::::::: :::::::::::::::::::::::::::::::::       ::::::::::::::::::: :::    :::::::::::::: '
 echo '     :+:       :+:    :+::+:       :+:       :+:       :+:           :+:   :+:    :+::+:    :+:    :+:      '
@@ -9,12 +11,21 @@ echo '###       ###    ##########################       ########################
 
 echo ''
 echo 'Select from options below'
-echo '1. Search aircraft registration        2. Search flight number (not active)'
-echo '3. Get flight history (not active)     4. Exit'
+echo '1. Search aircraft registration        2. Search flight number'
+echo '3. Run install script (run with sudo)  4. Exit'
 echo ''
 printf 'Selection: '
 read -r selection
 if [ "$selection" = "1" ]; then
-	echo 'Selected option 1.'
-	bash ./modules/aircraft-registration.sh
+        echo 'Selected option 1.'
+        bash ./modules/aircraft-registration.sh
+elif [ "$selection" = "2" ]; then
+        echo 'This module is not currently active, sorry.'
+        exit 1
+elif [ "$selection" = "3" ]; then
+        echo 'Selected option 3.'
+        bash ./modules/install.sh
+elif [ "$selection" = "4" ]; then
+        echo 'Exiting, thank you for using FreeFlight!'
+        exit 0
 fi
